d22f5e22cc4a3a9dd2c297ed851f7a658c8d83f4
Sebastian Chumaceiro <sebcr99@gmail.com>
2022-06-06 14:08:30 +0100

    Dynamic programming exercises 2/5

--- week-5/1_money_change_again/change_dp.cpp
+++ week-5/1_money_change_again/change_dp.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <vector>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+int get_change(int m)
+{
+  if (m < 0)
+    return 0;
+  std::vector<int> d = {1, 3, 4};
+  std::vector<int> change(m + 1, INT8_MAX);
+  change[0] = 0; // 0 coins used to change 0
+  int numCoins;
+
+  for (int i = 1; i < change.size(); ++i)
+  {
+    for (int j = 0; j < d.size(); ++j)
+    {
+      if (i >= d[j])
+      {
+        numCoins = change[i - d[j]] + 1;
+        if (numCoins < change[i])
+        {
+          change[i] = numCoins;
+        }
+      }
+    }
+  }
+  return change[m];
 }
 
-int main() {
+int main()
+{
   int m;
   std::cin >> m;
   std::cout << get_change(m) << '\n';
--- week-5/2_primitive_calculator/primitive_calculator.cpp
+++ week-5/2_primitive_calculator/primitive_calculator.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using std::vector;
 
-vector<int> optimal_sequence(int n) {
+vector<int> optimal_sequence(int n)
+{
   std::vector<int> sequence;
-  while (n >= 1) {
+  while (n >= 1)
+  {
     sequence.push_back(n);
-    if (n % 3 == 0) {
+    if (n % 3 == 0)
+    {
       n /= 3;
-    } else if (n % 2 == 0) {
+    }
+    else if (n % 2 == 0)
+    {
       n /= 2;
-    } else {
+    }
+    else
+    {
       n = n - 1;
     }
   }
@@ -20,12 +28,47 @@ vector<int> optimal_sequence(int n) {
   return sequence;
 }
 
-int main() {
+vector<int> dp_sequence(int n)
+{
+  vector<int> v(n + 1, 0);
+  v[1] = 0;
+  v[2] = 1;
+  v[3] = 1;
+
+  for (int i = 4; i < n + 1; ++i) {
+    int min = INT8_MAX;
+    if (i % 3 == 0) {
+      // odd and divisible by 3?
+      min = v[i/3] < min ? v[i/3] : min;
+    } else if (i % 2 == 0) {
+      // even and divisible by 2?
+      min = v[i/2] < min ? v[i/2] : min;
+    }
+    min = v[i-1] < min ? v[i-1] : min;
+    
+    v[i] = min + 1;
+  }
+
+  vector<int> sequence;
+  while (n > 1) {
+    sequence.push_back(n);
+    if (v[n-1] == v[n] - 1) --n;
+    else if (n % 2 == 0 && v[n/2] == v[n] - 1) n /= 2;
+    else if (n % 3 == 0 && v[n/3] == v[n] - 1) n /= 3;
+  }
+  sequence.push_back(1);
+  reverse(sequence.begin(), sequence.end());
+  return sequence;
+}
+
+int main()
+{
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
+  vector<int> sequence = dp_sequence(n);
   std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
+  for (size_t i = 0; i < sequence.size(); ++i)
+  {
     std::cout << sequence[i] << " ";
   }
 }
